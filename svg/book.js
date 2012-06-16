1b045790e551dd62d0c0f01bd5af4a17b270a017
Lê Hoàng Phương <herophuong93@gmail.com>
2012-06-16 22:20:43 +0700

    complete flip next effect

--- svg/book.js
+++ svg/book.js
@@ -1,6 +1,7 @@
 var mouseDragging = false; // indicate dragging state
 var mouseOffSetX = 0;      // indicate offset for proper dragging object
 var dragX = 0;
+var actedFlipNext = true;
 // When mouse button presses
 function mouseDown(evt) {
    // initiate dragging state
@@ -16,6 +17,7 @@ function mouseDown(evt) {
       mouseOffSetX = p.x - dragX;
    }
    
+   // clear all selection to avoid dragging text bug
    if (window.getSelection) {
    if (window.getSelection().empty) {  // Chrome
       window.getSelection().empty();
@@ -30,6 +32,12 @@ function mouseDown(evt) {
 function mouseUp(evt) {
    // stop dragging state
    mouseDragging = false;
+   // move the center line up after done flipping
+   if (document.getElementById("next-left-content").getAttribute("transform") == "translate(280,330)") {
+      var centerLine = document.getElementById("center-line");
+      centerLine.parentNode.appendChild(centerLine);
+      doneFlippingNext();
+   }
 }
 
 // When mouse button moving
@@ -38,6 +46,7 @@ function mouseMove(evt) {
    var p = document.documentElement.createSVGPoint();
    p.x = evt.clientX;
    p.y = evt.clientY;
+   actedFlipNext = true;
    
    if (mouseDragging) {
       // get needed objects
@@ -56,21 +65,22 @@ function mouseMove(evt) {
       // convert mouse position to svg coordinate
       p = p.matrixTransform(CTM.inverse());
       dragX = p.x - mouseOffSetX;
-      if ( (p.x-mouseOffSetX) > 0 && (p.x-mouseOffSetX) < 260) {
-         sLine.setAttribute("transform", "translate(" + dragX + ",0) rotate(" + 45*(p.x-mouseOffSetX)/260 +")");
-         hideRightMask.setAttribute("transform", "translate(" + dragX + ",0) rotate(" + 45*(p.x-mouseOffSetX)/260 +")");
-         nextLeftContent.setAttribute("transform", "translate(" + (p.x - mouseOffSetX + 280) + ",330) rotate(" + 90*(p.x-mouseOffSetX)/260 +") translate(" + dragX + ",0)");
-         revealLeftMask.setAttribute("transform", "translate(" + dragX + ",0) rotate(" + 45*(p.x-mouseOffSetX)/260 +")");
-      } else if ( (p.x-mouseOffSetX) >= 260) {
+      if ( (dragX) > 0 && (dragX) < 260) {
+         sLine.setAttribute("transform", "translate(" + dragX + ",0) rotate(" + 45*(dragX)/260 +")");
+         hideRightMask.setAttribute("transform", "translate(" + dragX + ",0) rotate(" + 45*(dragX)/260 +")");
+         nextLeftContent.setAttribute("transform", "translate(" + (dragX + 280) + ",330) rotate(" + 90*(dragX)/260 +") translate(" + dragX + ",0)");
+         revealLeftMask.setAttribute("transform", "translate(" + dragX + ",0) rotate(" + 45*(dragX)/260 +")");
+      } else if ( (dragX) >= 260) {
          sLine.setAttribute("transform", "translate(260,0) rotate(45)");
-         revealLeftMask.setAttribute("transform", "translate(260,0) rotate(45)");
+         revealLeftMask.removeAttribute("transform");
          hideRightMask.setAttribute("transform", "translate(260,0) rotate(45)");
          nextLeftContent.setAttribute("transform", "translate(540,330) rotate(90) translate(260,0)");
       } else {
-         sLine.setAttribute("transform", "");
-         hideRightMask.setAttribute("transform", "");
-         revealLeftMask.setAttribute("transform", "");
+         sLine.removeAttribute("transform");
+         hideRightMask.removeAttribute("transform");
+         revealLeftMask.removeAttribute("transform");
          nextLeftContent.setAttribute("transform", "translate(280,330)");
+         actedFlipNext = false;
       }
    }
 }
@@ -91,7 +101,60 @@ function reAttachEvent(){
    currentRightContent.addEventListener("mouseup", mouseUp, false);
    currentRightContent.addEventListener("mousemove", mouseMove, false);
 }
-function beginRightPageHoverEffect(){
+function doneFlippingNext(){
+   // prevent trigger again
+   if (actedFlipNext)
+      return;
+   
+   /* Restructure the right pages */
+   var nextRight = document.getElementById("next-right-container");
+   var currentRight = document.getElementById("current-right-container");
+   var previousRight = document.getElementById("previous-right-container");
+   
+   nextRight.parentNode.insertBefore(previousRight, nextRight);
+   previousRight.parentNode.insertBefore(currentRight, previousRight);
+   // next to current
+   nextRight.setAttribute("id", "current-right-container");
+   document.getElementById("hideCurrentRightPage-mask").setAttribute("transform", "translate(260,0) rotate(45)");
+   nextRight.setAttribute("mask", "url(#hideCurrentRightPage)");
+   nextRight.firstElementChild.setAttribute("id", "current-right-content");
+   nextRight.firstElementChild.firstElementChild.setAttribute("id", "current-right-page");
+   // previous to next
+   previousRight.setAttribute("id", "next-right-container");
+   previousRight.firstElementChild.setAttribute("id", "next-right-content");
+   previousRight.firstElementChild.firstElementChild.setAttribute("id", "next-right-page");
+   // current to previous
+   currentRight.setAttribute("id", "previous-right-container");
+   currentRight.removeAttribute("mask");
+   currentRight.firstElementChild.setAttribute("id", "previous-right-content");
+   currentRight.firstElementChild.firstElementChild.setAttribute("id", "previous-right-page");
+   
+   /* Restructure the left pages */
+   var nextLeft = document.getElementById("next-left-container");
+   var currentLeft = document.getElementById("current-left-container");
+   var previousLeft = document.getElementById("previous-left-container");
+   
+   nextLeft.parentNode.insertBefore(previousLeft, nextLeft);
+   previousLeft.parentNode.insertBefore(currentLeft, previousLeft);
+   // next to current
+   nextLeft.setAttribute("id", "current-left-container");
+   nextLeft.removeAttribute("mask");
+   nextLeft.firstElementChild.setAttribute("id", "current-left-content");
+   nextLeft.firstElementChild.firstElementChild.setAttribute("id", "current-left-page");
+   // previous to next
+   previousLeft.setAttribute("id", "next-left-container");
+   previousLeft.setAttribute("mask", "url(#revealNextLeftPage)");
+   previousLeft.firstElementChild.setAttribute("id", "next-left-content");
+   previousLeft.firstElementChild.firstElementChild.setAttribute("id", "next-left-page");
+   // current to previous
+   currentLeft.setAttribute("id", "previous-left-container");
+   currentLeft.firstElementChild.setAttribute("id", "previous-left-content");
+   currentLeft.firstElementChild.firstElementChild.setAttribute("id", "previous-left-page");
+   
+   document.getElementById("symmetry-line").setAttribute("transform", "translate(260,0) rotate(45)");
+   dragX = 260;
+   reAttachEvent();
+   actedFlipNext = true;
 }
 function init() {
    var nextLeftContent = document.getElementById("next-left-content");
